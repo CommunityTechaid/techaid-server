df34c4ad8a7c6983b126b4193af3c96c59b11506
Akhil Muraleedharan <akhil.hardys@gmail.com>
2024-03-26 17:14:35 +0000

    Added referringOrganisationConnection query for autocomplete

--- src/main/kotlin/cta/app/graphql/queries/ReferringOrganisationQueries.kt
+++ src/main/kotlin/cta/app/graphql/queries/ReferringOrganisationQueries.kt
@@ -5,6 +5,8 @@ import cta.app.ReferringOrganisation
 import cta.app.ReferringOrganisationRepository
 import cta.app.graphql.filters.ReferringOrganisationWhereInput
 import cta.graphql.KeyValuePair
+import cta.graphql.PaginationInput
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Sort
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Component
@@ -25,6 +27,14 @@ class ReferringOrganisationQueries(
         }
     }
 
+    @PreAuthorize("hasAnyAuthority('app:admin', 'read:organisations')")
+    fun referringOrganisationsConnection(page: PaginationInput?, where: ReferringOrganisationWhereInput?): Page<ReferringOrganisation> {
+        val f: PaginationInput = page ?: PaginationInput()
+        if (where == null) {
+            return referringOrganisations.findAll(f.create())
+        }
+        return referringOrganisations.findAll(where.build(), f.create())
+    }
     @PreAuthorize("hasAnyAuthority('app:admin', 'read:organisations')")
     fun referringOrganisation(where: ReferringOrganisationWhereInput): Optional<ReferringOrganisation> = referringOrganisations.findOne(where.build())
 }
