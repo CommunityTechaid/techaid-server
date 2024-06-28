fa90c68a9a8a0b3474af5b70bb8d58a1970d2afe
Tony Anzelmo <tony.anzelmo@gmail.com>
2024-06-28 16:50:10 +0100

    Org v2 - more missing refs

--- src/main/kotlin/cta/app/graphql/queries/ReferringOrganisationContactQueries.kt
+++ src/main/kotlin/cta/app/graphql/queries/ReferringOrganisationContactQueries.kt
@@ -6,6 +6,7 @@ import cta.app.ReferringOrganisationContactRepository
 import cta.app.graphql.filters.ReferringOrganisationContactWhereInput
 import cta.graphql.KeyValuePair
 import cta.graphql.PaginationInput
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Sort
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Component
