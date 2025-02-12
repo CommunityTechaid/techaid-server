051737cbe4ee452696913799536190fd2b1567f7
Akhil Muraleedharan <akhil.hardys@gmail.com>
2025-02-12 16:37:23 +0000

    Created CORS Config to allow requests from app-testing. Currently hardcoded in the configuration for testing. Need to move to application.yml file or env.

---
+++ src/main/kotlin/cta/app/config/CorsConfig.kt
@@ -0,0 +1,28 @@
+package cta.app.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/graphql")
+                    .allowedOrigins("https://app-testing.communitytechaid.org.uk")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+
+    }
+
+
+}
