5cb9b43094c233d862e5caa4bf1bd271032c050e
Akhil Muraleedharan <akhil.hardys@gmail.com>
2025-06-26 16:31:09 +0100

    Added custom scalar for Instant instead of String

--- src/main/kotlin/cta/app/config/GraphQlConfig.kt
+++ src/main/kotlin/cta/app/config/GraphQlConfig.kt
@@ -1,20 +1,75 @@
 package cta.app.config
 
+import graphql.GraphQLContext
+import graphql.execution.CoercedVariables
+import graphql.language.Value
 import graphql.scalars.ExtendedScalars
+import graphql.schema.*
 import graphql.schema.idl.RuntimeWiring
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.graphql.execution.RuntimeWiringConfigurer
+import java.time.Instant
+import java.time.format.DateTimeParseException
+import java.util.*
 
 
 @Configuration
 public class GraphQlConfig {
+
     @Bean
-    fun runtimeWiringConfigurer(): RuntimeWiringConfigurer {
+    fun runtimeWiringConfigurer(instantScalar: GraphQLScalarType): RuntimeWiringConfigurer {
         return RuntimeWiringConfigurer { wiringBuilder: RuntimeWiring.Builder ->
             wiringBuilder
                 .scalar(ExtendedScalars.GraphQLBigDecimal)
                 .scalar(ExtendedScalars.GraphQLLong)
+                .scalar(instantScalar)
+
+
         }
     }
+}
+
+
+@Configuration
+class GraphQLScalarConfig {
+
+    @Bean
+    fun instantScalar(): GraphQLScalarType {
+        return GraphQLScalarType.Builder()
+            .name("Instant")
+            .description("A custom scalar that handles java.time.Instant in ISO-8601 format")
+            .coercing(object : Coercing<Instant, String> {
+
+                override fun serialize(dataFetcherResult: Any, graphQLContext: GraphQLContext, locale: Locale): String {
+                    return when (dataFetcherResult) {
+                        is Instant -> dataFetcherResult.toString()
+                        else -> throw CoercingSerializeException("Expected Instant object.")
+                    }
+                }
+
+                override fun parseValue(input: Any, graphQLContext: GraphQLContext, locale: Locale): Instant {
+                    return try {
+                        Instant.parse(input.toString())
+                    } catch (e: DateTimeParseException) {
+                        throw CoercingParseValueException("Invalid ISO date-time: $input")
+                    }
+                }
+
+                override fun parseLiteral(
+                    input: Value<*>,
+                    variables: CoercedVariables,
+                    graphQLContext: GraphQLContext,
+                    locale: Locale
+                ): Instant? {
+                    return try {
+                        Instant.parse(input.toString())
+                    } catch (e: DateTimeParseException) {
+                        throw CoercingParseLiteralException("Invalid ISO date-time: $input")
+                    }
+                }
+            })
+            .build()
+    }
+
 }
