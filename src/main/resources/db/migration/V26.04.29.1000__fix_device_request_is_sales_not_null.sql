01b80dcc52f96644186de56d99e6999e4203f502
Tony Anzelmo <tony.anzelmo@gmail.com>
2026-04-29 07:53:35 +0100

    Update V26.04.29.1000__fix_device_request_is_sales_not_null.sql

--- src/main/resources/db/migration/V26.04.29.1000__fix_device_request_is_sales_not_null.sql
+++ src/main/resources/db/migration/V26.04.29.1000__fix_device_request_is_sales_not_null.sql
@@ -1,3 +1,8 @@
-UPDATE device_requests SET is_sales = false WHERE is_sales IS NULL;
-ALTER TABLE device_requests ALTER COLUMN is_sales SET NOT NULL;
-ALTER TABLE device_requests ALTER COLUMN is_sales SET DEFAULT false;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'device_requests') THEN
+        UPDATE device_requests SET is_sales = false WHERE is_sales IS NULL;
+        ALTER TABLE device_requests ALTER COLUMN is_sales SET NOT NULL;
+        ALTER TABLE device_requests ALTER COLUMN is_sales SET DEFAULT false;
+    END IF;
+END $$;
